49447c063bdc59baedef5a5bc504886e398a80b9
pankajmokashi <pankajmokasi12@gmail.com>
2023-07-25 18:38:06 +0530

    submitting code-Tue Jul 25 2023 18:38:03 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
 
-export default App
+  let [state, setState] = useState("")
+  let [data, setData] = useState("")
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.products)
+        setData(data.products)
+        setState("fetched");
+      })
+      .catch((error) =>
+        console.error("Error fetching data:", error)
+      );
+  }, []);
+
+  return (
+  <div>
+    {/* Do not remove the main div */}
+      {
+        data &&
+        data.map(ele => (
+          <pre>{JSON.stringify(ele)}</pre>
+        ))
+      }
+  </div>);
+};
+
+export default App;
